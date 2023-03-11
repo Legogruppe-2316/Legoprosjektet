b68d693911e5b757ca7a5709b56b7495348102de
gardgundersen44 <gardgundersen4@gmail.com>
2023-03-11 10:27:09 +0100

    Rydding i kode

--- Prosjekt01_NumeriskIntegrasjon/Prosjekt01_NumeriskIntegrasjon.m
+++ Prosjekt01_NumeriskIntegrasjon/Prosjekt01_NumeriskIntegrasjon.m
@@ -18,49 +18,26 @@
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %                EXPERIMENT SETUP AND DATA FILENAME
 %
-% Alltid lurt å rydde workspace opp først
 clear; close all
-% Skal prosjektet gjennomføres online mot EV3 eller mot lagrede data?
-online = false;
-% Spesifiser et beskrivende filnavn for lagring av måledata
-filename = 'P01_NumeriskIntegrasjon_sinus.mat';
+online = true;
+
+% Lagring av måledata
+filename = 'P0X_MeasBeskrivendeTekst_Y.mat';
 %--------------------------------------------------------------------------
 
 
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %                      INITIALIZE EQUIPMENT
-% Initialiser styrestikke, sensorer og motorer.
-%
-% Spesifiser hvilke sensorer og motorer som brukes.
-% I Matlab trenger du generelt ikke spesifisere porten de er tilkoplet.
-% Unntaket fra dette er dersom bruke 2 like sensorer, hvor du må
-% initialisere 2 sensorer med portnummer som argument.
-% Eksempel:
- %mySonicSensor_1 = sonicSensor(mylego,3);
-% mySonicSensor_2 = sonicSensor(mylego,4);
-
-% For ryddig og oversiktlig kode, kan det være lurt å slette
-% de sensorene og motoren som ikke brukes. 
-
 if online
-    
-    % LEGO EV3 og styrestikke    
+    % LEGO EV3 og styrestikke
     mylego = legoev3('USB');
-    selected_joystick = 1;
-    if ~ismac && isunix
-        selected_joystick = 2;
-    end
-    disp(selected_joystick)
-    joystick = vrjoystick(selected_joystick);
+    joystick = vrjoystick(1);
     [JoyAxes,JoyButtons] = HentJoystickVerdier(joystick);
-
     % sensorer
     myColorSensor = colorSensor(mylego);
-
 else
     % Dersom online=false lastes datafil.
     load(filename)
-    online = false;
 end
 
 disp('Equipment initialized.')
@@ -84,14 +61,8 @@ k=1;
 
 while ~JoyMainSwitch
     %+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    %                       GET TIME AND MEASUREMENT
-    % Få tid og målinger fra sensorer, motorer og joystick
-    %
-    % For ryddig og oversiktlig kode, kan det være lurt å slette
-    % de sensorene og motoren som ikke brukes.
-   
+    %                       GET TIME AND MEASUREMENT   
     if online
-        disp("Hei")
         if k==1
             tic
             Tid(1) = 0;
@@ -102,17 +73,13 @@ while ~JoyMainSwitch
             Lys(k) = double(readLightIntensity(myColorSensor,'reflected'));
         end
 
-        % Data fra styrestikke. Utvid selv med andre knapper og akser.
-        % Bruk filen joytest.m til å finne koden for de andre 
-        % knappene og aksene.
+        % Hent data fra joystick
         [JoyAxes,JoyButtons] = HentJoystickVerdier(joystick);
         JoyMainSwitch = JoyButtons(1);
         JoyForover(k) = JoyAxes(2);
 
     else
         % online=false
-        % Når k er like stor som antall elementer i datavektoren Tid,
-        % simuleres det at bryter på styrestikke trykkes inn.
         if k==numel(Tid)
             JoyMainSwitch=1;
         end
@@ -131,45 +98,23 @@ while ~JoyMainSwitch
 
     % Parametre
     a=0.7;
+
     % Tilordne målinger til variabler'
     nullflow = Lys(1); %nullpunkt for reflektert lys
-    y(1) = 20; %volum
+    y(1) = 0; %volum
     Ts(1) = 0;
-    Offset = 5;
     Flow(1) = Lys(1) - nullflow;
    
     % Regner ut datavektorene lys, tid, flow og volum
     if(k>=2)
         Ts(k) = Tid(k) - Tid(k-1);
-        Flow(k) = nullflow - Lys(k) - 13;
+        Flow(k) = Lys(k) - nullflow;
         y(k) = y(k-1) + Ts(k) * Flow(k-1);
     end
-
-    % Andre beregninger som ikke avhenger av initialverdi
-    
-
-    % Pådragsberegninger
-    %PowerA(k) = a*JoyForover(k);
-
-    %if online
-        % Setter powerdata mot EV3
-        %motorA.Speed = PowerA(k);
-        %start(motorA);
-    %end
     %--------------------------------------------------------------
-
-
-
 
     %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     %                  PLOT DATA
-    % Denne seksjonen plasseres enten i while-lokka eller rett etterpå.
-    % Dette kan enkelt gjøres ved flytte de 5 nederste linjene
-    % før "end"-kommandoen nedenfor opp før denne seksjonen.
-    %
-    % Husk at syntaksen plot(Tid(1:k),data(1:k))
-    % gir samme opplevelse i online=0 og online=1 siden
-    % hele datasettet (1:end) eksisterer i den lagrede .mat fila
 
     % aktiver fig1
     figure(fig1)
@@ -187,13 +132,6 @@ while ~JoyMainSwitch
     % tegn nå (viktig kommando)
     drawnow
     %--------------------------------------------------------------
-
-    % For å flytte PLOT DATA etter while-lokken, er det enklest å
-    % flytte de neste 5 linjene (til og med "end") over PLOT DATA.
-    % For å indentere etterpå, trykk Ctrl-A/Cmd-A og deretter
-    % Crtl-I/Cmd-Ixxx
-    %
-    % Oppdaterer tellevariabel
     k=k+1;
 end
 
